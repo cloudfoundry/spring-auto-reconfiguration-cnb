62dbcd0ad0e7ae35cc60d4f8e018fa5a4b5ef669
Ben Hale <bhale@pivotal.io>
2019-06-07 15:08:06 -0700

    Normalize version
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/dependency.sh
+++ ci/dependency.sh
@@ -11,11 +11,25 @@ commit() {
   git commit -a -m "Dependency Upgrade: $1 $2" || true
 }
 
+version() {
+  local PATTERN="([0-9]+)\.([0-9]+)\.([0-9]+)-(.*)"
+
+  for VERSION in $(cat $1); do
+      if [[ ${VERSION} =~ ${PATTERN} ]]; then
+        echo "${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}"
+        return
+      else
+        >2 echo "version is not semver"
+        exit 1
+      fi
+    done
+}
+
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 git config --local user.name "$GIT_USER_NAME"
 git config --local user.email ${GIT_USER_EMAIL}
 
 go build -ldflags='-s -w' -o bin/dependency github.com/cloudfoundry/libcfbuildpack/dependency
 
-bin/dependency auto-reconfiguration "[\d]+\.[\d]+\.[\d]+" $(cat ../auto-reconfiguration/version) $(cat ../auto-reconfiguration/uri)  $(cat ../auto-reconfiguration/sha256)
-commit auto-reconfiguration $(cat ../auto-reconfiguration/version)
+bin/dependency auto-reconfiguration "[\d]+\.[\d]+\.[\d]+" $(version ../auto-reconfiguration/version) $(cat ../auto-reconfiguration/uri)  $(cat ../auto-reconfiguration/sha256)
+commit auto-reconfiguration $(version ../auto-reconfiguration/version)
